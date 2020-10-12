98d9680ba5d41c1a11a2d961dac756ac706f9d2f
tetelab <tetelabtt@gmail.com>
2020-10-12 17:02:55 +0900

    Initial Commit

---
+++ SlideshowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideshowApp
+//
+//  Created by 松岡哲平 on 2020/10/12.
+//  Copyright © 2020 tetelab. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
