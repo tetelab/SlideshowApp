aec2cca6a12059597ff8182bf217d733031f81ef
tetelab <tetelabtt@gmail.com>
2020-10-25 15:47:07 +0900

    質問用

---
+++ SlideshowApp/ExpansionViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ExpansionViewController.swift
+//  SlideshowApp
+//
+//  Created by 松岡哲平 on 2020/10/25.
+//  Copyright © 2020 tetelab. All rights reserved.
+//
+
+import UIKit
+
+class ExpansionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,12 +9,95 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var btnStartStop: UIButton!
+    @IBOutlet weak var btnReturn: UIButton!
+    @IBOutlet weak var btnMoveOn: UIButton!
+    
+    // 配列に指定するindex番号を宣言
+    var nowIndex:Int = 0
+    
+    // スライドショーに使用するタイマーを宣言
+    var timer: Timer!
+    
+    // スライドショーさせる画像の配列を宣言
+    var imageArray:[UIImage] = [
+        UIImage(named: "Ham1")!,
+        UIImage(named: "Ham2")!,
+        UIImage(named: "Ham3")!
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = imageArray[nowIndex]
+    }
+    
+    @IBAction func slideShowBtn(_ sender: Any) {
+        // 再生中か停止しているかを判定
+        if (timer == nil) {
+            // 再生時の処理を実装
+
+            // タイマーをセットする
+            timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(changeImage), userInfo: nil, repeats: true)
+
+            // ボタンの名前を停止に変える
+            btnStartStop.setTitle("停止", for: .normal)
+            
+            //戻る、進むボタンを選択できないように
+            btnReturn.isEnabled = false
+            btnMoveOn.isEnabled = false
+
+        } else {
+            // 停止時の処理を実装
+            // タイマーを停止する
+            timer.invalidate()
+
+            // タイマーを削除(timer.invalidateだけだとtimerがnilにならないため)
+            timer = nil
+
+            // ボタンの名前を再生に直しておく
+            btnStartStop.setTitle("再生", for: .normal)
+            
+            //戻る、進むボタンを選択できるように
+            btnReturn.isEnabled = true
+            btnMoveOn.isEnabled = true
+        }
     }
 
+    @IBAction func returnBtn(_ sender: Any) {
+        returnImage()
+    }
+    
+    @IBAction func moveOnBtn(_ sender: Any) {
+        changeImage()
+    }
+    
+    
+    @objc func changeImage() {
+        // indexを増やして表示する画像を切り替える
+        nowIndex += 1
 
+        // indexが表示予定の画像の数と同じ場合
+        if (nowIndex >= imageArray.count) {
+            // indexを一番最初の数字に戻す
+            nowIndex = 0
+        }
+        // indexの画像をstoryboardの画像にセットする
+        imageView.image = imageArray[nowIndex]
+    }
+    
+    @objc func returnImage() {
+           // indexを減らして表示する画像を切り替える
+           nowIndex -= 1
+
+           // indexがマイナスになってしまう場合
+           if (nowIndex <= 0) {
+               // indexを一番最後の数字に戻す
+               nowIndex = imageArray.count
+           }
+           // indexの画像をstoryboardの画像にセットする
+           imageView.image = imageArray[nowIndex - 1]
+       }
 }
 
